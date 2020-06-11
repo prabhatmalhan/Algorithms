7e3b8689b4a7379231ba2271942a6b8e08e381bf
prabhatmalhan <prabhat.malhan7@gmail.com>
2020-06-11 17:39:56 +0530

    Create SelectionSort.c

---
+++ SelectionSort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void SelectionSort(int *a,int n)
+{
+    for(int i=0;i<n-1;++i)
+    {
+        int sm = i;
+        for(int j=i+1 ; j<n ; ++j)
+            if(a[sm]>a[j])
+              sm = j;
+        int temp = a[sm];
+        a[sm]=a[i];
+        a[i]=temp;
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0 ; i<n ; ++i)
+        scanf("%d",&arr[i]);
+    SelectionSort(arr,n);
+    for(int i=0 ; i<n ; ++i)
+        printf("%d ",arr[i]);
+    return 0;
+}
