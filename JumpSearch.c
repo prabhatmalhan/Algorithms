bd06eb21936183bb349892c1b6b7987005c71c0e
Prabhat Malhan <54873382+prabhatmalhan@users.noreply.github.com>
2020-06-18 11:08:05 +0530

    Add files via upload

---
+++ JumpSearch.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <math.h>
+
+int jumpSearch(int arr[],int key,int l,int n,int j)
+{
+    if(l>=n)
+        return 0;
+    else
+    {
+        if(l+j < n && arr[l+j]>key)
+        {
+            for(int i=l;i<l+j;++i)
+                if(arr[i]==key)
+                    return 1;
+            return 0;
+        }
+    }
+    jumpSearch(arr,key,l+j,n,j);
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0 ; i<n ; ++i)
+        scanf("%d",&arr[i]);
+    int k;
+    scanf("%d",&k);
+    if(jumpSearch(arr,k,0,n,sqrt(n)))
+        printf("Found");
+    else
+        printf("Not found");
+    return 0;
+}
