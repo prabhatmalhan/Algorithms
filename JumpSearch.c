ad7e347b69ac5e2531026114fae51bc8cca9a683
Prabhat Malhan <54873382+prabhatmalhan@users.noreply.github.com>
2020-06-18 11:28:11 +0530

    Update JumpSearch.c

--- JumpSearch.c
+++ JumpSearch.c
@@ -7,9 +7,12 @@ int jumpSearch(int arr[],int key,int l,int n,int j)
         return 0;
     else
     {
-        if(l+j < n && arr[l+j]>key)
+        int x = l+j;
+        if(x>n)
+            x=n;
+        if(arr[x]>key)
         {
-            for(int i=l;i<l+j;++i)
+            for(int i=l;i<x;++i)
                 if(arr[i]==key)
                     return 1;
             return 0;
