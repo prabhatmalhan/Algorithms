266c8c62f15c14d0253979ce830b6e0024d106a5
Prabhat Malhan <prabhat.malhan7@gmail.com>
2020-10-27 07:35:18 +0530

    Sorting Algorithm

--- BubbleSort.c
+++ BubbleSort.c
@@ -23,6 +23,7 @@ int main()
     for(int i=0 ; i<n ; ++i)
         scanf("%d",&arr[i]);
     BubbleSort(arr,n);
+    //array sorted
     for(int i=0 ; i<n ; ++i)
         printf("%d ",arr[i]);
     return 0;
